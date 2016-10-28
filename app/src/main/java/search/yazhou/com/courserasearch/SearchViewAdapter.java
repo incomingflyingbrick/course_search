1f2226fb8c43eb85f55c46928be4eb8ba2e64a73
vurtne <vurtnesaerdna@gmail.com>
2016-10-28 18:30:03 +0800

    fixing list

--- app/src/main/java/search/yazhou/com/courserasearch/SearchViewAdapter.java
+++ app/src/main/java/search/yazhou/com/courserasearch/SearchViewAdapter.java
@@ -2,8 +2,11 @@ package search.yazhou.com.courserasearch;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -14,10 +17,11 @@ import java.util.List;
 public class SearchViewAdapter extends RecyclerView.Adapter<SearchViewAdapter.SearchViewHolder> {
 
     private Context mContext;
-
+    private static final int COURSE = 2;
+    private static final int SPECIALIZATION = 1;
     private List<Model> mModelList;
 
-    public SearchViewAdapter(Context context , List<Model> dataList){
+    public SearchViewAdapter(Context context, List<Model> dataList) {
         mContext = context;
         mModelList = dataList;
     }
@@ -29,13 +33,18 @@ public class SearchViewAdapter extends RecyclerView.Adapter<SearchViewAdapter.Se
 
     @Override
     public int getItemViewType(int position) {
-        return super.getItemViewType(position);
+        if(mModelList.get(position) instanceof Course){
+            return COURSE;
+        }else if(mModelList.get(position) instanceof Specialization){
+            return SPECIALIZATION;
+        }
+        return 1;
     }
 
     @Override
     public SearchViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
-        return null;
+        LayoutInflater inflater =(LayoutInflater) parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        return new SearchViewHolder(inflater.inflate(R.layout.course_list,parent,false));
     }
 
     @Override
@@ -43,9 +52,19 @@ public class SearchViewAdapter extends RecyclerView.Adapter<SearchViewAdapter.Se
 
     }
 
-    static class SearchViewHolder extends RecyclerView.ViewHolder{
+    static class SearchViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView mImageView;
+        TextView mCourseTextView;
+        TextView mUniversityTextView;
+        TextView mCourseCountTextView;
+
         public SearchViewHolder(View itemView) {
             super(itemView);
+            mImageView = (ImageView) itemView.findViewById(R.id.course_iv);
+            mCourseTextView = (TextView) itemView.findViewById(R.id.course_name_tv);
+            mCourseCountTextView = (TextView) itemView.findViewById(R.id.course_counte_tv);
+            mUniversityTextView = (TextView) itemView.findViewById(R.id.course_uni_name_tv);
         }
     }
 }
